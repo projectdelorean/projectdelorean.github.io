fbae7195ab5101d3e70cfde144f3143302535a02
Vivek Vinodh <vivekvinodh@users.noreply.github.com>
2018-12-15 15:02:55 -0800

    finish glasses page design, add scroll reveal for animations

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -236,4 +236,150 @@ $('document').ready(function () {
             scrollTop: $(".glasses-page-design-section").offset().top
         }, 2000);
     });
+    
+    $('.freeway-use-case').on('click', function (event) {
+        console.log("ASKDLAS")
+        $("#freeway-collapse").collapse("toggle");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    
+    $('.concert-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("toggle");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.baby-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("toggle");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.whale-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("toggle");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.funny-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("toggle");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.social-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("toggle");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.daughter-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("toggle");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.runyon-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("toggle");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.warriors-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("toggle");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.moment-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("toggle");
+        $("#anytime-collapse").collapse("hide");
+    });
+    $('.anytime-use-case').on('click', function (event) {
+        $("#freeway-collapse").collapse("hide");
+        $("#concert-collapse").collapse("hide");
+        $("#baby-collapse").collapse("hide");
+        $("#whale-collapse").collapse("hide");
+        $("#funny-collapse").collapse("hide");
+        $("#social-collapse").collapse("hide");
+        $("#daughter-collapse").collapse("hide");
+        $("#runyon-collapse").collapse("hide");
+        $("#warriors-collapse").collapse("hide");
+        $("#moment-collapse").collapse("hide");
+        $("#anytime-collapse").collapse("toggle");
+    });
 });
