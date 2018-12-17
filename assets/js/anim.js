d413f7945ab1bb31c76a0f163f34a8dbb81d831b
Vivek Vinodh <vivekvinodh@users.noreply.github.com>
2018-12-17 12:02:27 -0800

    remove animations, move code to separate file.

---
+++ assets/js/anim.js
@@ -0,0 +1,189 @@
+    ScrollReveal().reveal('.main-page', {
+        duration: 750
+    });
+    ScrollReveal().reveal('.main-page-title-text', {
+        delay: 750,
+        origin: 'bottom',
+        distance: '60px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.main-page-desc-text', {
+        delay: 1000,
+        duration: 750
+
+    });
+    ScrollReveal().reveal('.main-page-tagline-text', {
+        delay: 1250,
+        duration: 750
+
+    });
+    ScrollReveal().reveal('.main-page-btn', {
+        delay: 1500,
+        duration: 750
+
+    });
+    ScrollReveal().reveal('.main-page-bottom-text', {
+        delay: 250,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-hero', {
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-title-text', {
+        delay: 750,
+        origin: 'bottom',
+        distance: '60px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-desc-text', {
+        delay: 1000,
+        duration: 750
+
+    });
+    ScrollReveal().reveal('.glasses-page-tagline-text', {
+        delay: 1250,
+        duration: 750
+
+    });
+    ScrollReveal().reveal('.glasses-page-hero-button-section', {
+        delay: 1500,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-hero-bottom', {
+        delay: 1750,
+        origin: 'top',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-section', {
+        delay: 100,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-title', {
+        delay: 100,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-title', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-title-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-simple', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-simple-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-sleek', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-sleek-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-stylish', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-stylish-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-design-btn', {
+        delay: 1000,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-section', {
+        delay: 100,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-title', {
+        delay: 100,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-title', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-title-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-simple', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-simple-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-sleek', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-sleek-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-stylish', {
+        delay: 250,
+        origin: 'left',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-stylish-desc', {
+        delay: 500,
+        origin: 'bottom',
+        distance: '50px',
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-function-btn', {
+        delay: 1000,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-use-section', {
+        delay: 100,
+        duration: 750
+    });
+    ScrollReveal().reveal('.glasses-page-bottom-text', {
+        delay: 250,
+        duration: 750
+    });
